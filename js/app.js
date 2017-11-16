73372958414c6482f1ada8b88f553d5bd5b4ee45
jessia2011 <jessicamezarojas2011@gmail.com>
2017-11-16 18:13:23 -0500

    version 0.0.3

--- js/app.js
+++ js/app.js
@@ -3,41 +3,47 @@ var post = document.querySelector('.post-js');
 var viewCount = document.querySelector('.count-js');
 var twett = document.querySelector('.container-text-js'); 
 var message = ''; 
-var maxLetter = 145; 
+var maxLetter = 140; 
 
 
 post.addEventListener('keydown', validateLength);
 post.addEventListener('keyup', validateLength);
-post.addEventListener('keyup',disabledBtn )
+//post.addEventListener('keyup',disabledBtn )
 button.addEventListener('click', twittear);
 
 
 function validateLength () {
      var numberLetters = post.value.length;
-     if(numberLetters>=maxLetter) {
-         post.value = message ;
-     }else {
-         message = post.value;
+     if(numberLetters <= 0 || numberLetters>maxLetter) {
+        button.disabled=true;
+        button.classList.remove('active-btn');
+        button.classList.add('btn');
+     }else if (numberLetters > 0 || numberLetters<maxLetter ) {
+         button.disabled=false;
+         button.classList.add('active-btn');
+         button.classList.remove('btn');
      }
-     viewCount.value = numberLetters;
+     
+     if (numberLetters>120 && numberLetters<130){
+         viewCount.style.color='green';
+     } else if (numberLetters>130 && numberLetters<=140){
+        viewCount.style.color='red';
+     } else  if (numberLetters<=120) {
+        viewCount.style.color='black';
+     }
+     viewCount.value = maxLetter - numberLetters;
+     
 }
 
 function twittear(event) { 
     //twett.innerHTML = post.value;  
     if(post.value) {
         var p = document.createElement('p');
-       
         p.textContent = post.value;
         twett.appendChild(p);
         post.value = '';
     }
 }
 
-function disabledBtn () {
-    if (post.value.length == 0 || post.value.length>140){
-        button.disabled=true;
-    } else if (post.value.length > 0 ) {
-        button.disabled=false;
-    }        
-}
+
 

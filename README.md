695d53822fa1278406b9cc9e1ebccf225df821c2
jessia2011 <jessicamezarojas2011@gmail.com>
2017-11-20 06:15:55 -0500

    reto resuelto, mejorar css

---
+++ README.md
@@ -0,0 +1,24 @@
+# Twitter
+
+* **Unidad:** _Maquetado web con HTML & CSS_
+
+***
+
+El reto consiste en replicar la página de  _Twitter_.
+
+## Objetivo
+
+Hacer uso de DOM y CSS, para replicar el siguiente resultado a lograr:
+
+![mini-twitter](assets/docs/twitter.gif)
+
+## Resultados
+
+* Cumple con los requisitos del resultado a lograr, además se añadio mejores estilos a la página, logrando una mejor visualización.
+
+* Se añadio la función `hora: 
+
+* El resultado fue el siguiente [sitio original](https://jessica2011.github.io/twitter/)
+
+![mini-twitter](assets/docs/captura-twitter.png)
+ 
--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ post.addEventListener('keydown', validateLength);
 post.addEventListener('keyup', validateLength);
 //post.addEventListener('keyup',disabledBtn )
 button.addEventListener('click', twittear);
+post.addEventListener('keydown', expand);
 
 
 function validateLength () {
@@ -23,13 +24,14 @@ function validateLength () {
          button.classList.add('active-btn');
          button.classList.remove('btn');
      }
-     
+     var a = post.value;
      if (numberLetters>120 && numberLetters<130){
          viewCount.style.color='green';
      } else if (numberLetters>130 && numberLetters<=140){
         viewCount.style.color='red';
+        a.style.backgroundColor = '#3366FF';
      } else  if (numberLetters<=120) {
-        viewCount.style.color='black';
+        viewCount.style.color='blue';
      }
      viewCount.value = maxLetter - numberLetters;
      
@@ -39,11 +41,34 @@ function twittear(event) {
     //twett.innerHTML = post.value;  
     if(post.value) {
         var p = document.createElement('p');
-        p.textContent = post.value;
+        p.textContent = post.value +'  ' +time();
         twett.appendChild(p);
         post.value = '';
     }
 }
 
 
+function expand(){
+var el = this;
+setTimeout(function(){
+el.style.cssText = 'height:auto; padding:0';
+el.style.cssText = 'height:' + el.scrollHeight + 'px';
+},0);
+}
 
+/*hora*/ 
+function time() {
+	var date = new Date();
+	var hours = date.getHours();
+	var min = date.getMinutes();
+	var time;
+	if (min < 10) {
+		min = '0' + min;
+	}
+	if (hours >= 12 && hours <= 24) {
+		time = hours + ':' + min + ' PM';
+	} else {
+		time = hours + ':' + min + ' AM';
+	}
+	return time;
+}
